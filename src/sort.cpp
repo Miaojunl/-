6c45c61d45e8ea7214b86c709f71eaef129a48ec
MJ Li <275614140@qq.com>
2015-06-28 20:15:59 +0800

    second

--- src/sort.cpp
+++ src/sort.cpp
@@ -6,16 +6,45 @@ class Allsort{
 	friend class Mysort;
 };
 
+void headadjust(int *a,int element,int size){
+	int lchild=2*element;
+	int rchild=2*element+1;
+	int max=element;
+	if(element<=size/2){
+		if(a[lchild-1]>a[max-1] && lchild <=size){
+			max=lchild;
+		}
+		if(a[rchild-1]>a[max-1] && rchild <= size){
+			max=rchild;
+		}
+		if(max!=element){
+			swap(a[element-1],a[max-1]);
+			headadjust(a,max,size);
+		}
+	}
+}
+void bighead(int *a,int size){
+	for(int i=size/2;i>=1;i--){
+		headadjust(a,i,size);
+	}
+}
 
+void headsort(int *a,int size){
+	bighead(a,size);
+	for(int i=size;i>=1;i--){
+		swap(a[0],a[i-1]);
+		headadjust(a,1,i-1);
+	}
+}
 
-/*class Headsort:public Allsort{
-  public:
-  void sort(int *a,int n){`
-  return;
-  }
+class Headsort:public Allsort{
+	public:
+		void sort(int *a,int n){
+			headsort(a,n);
+			cout<<"this is head sort"<<endl;
+		}
+};
 
-  };
- */
 
 void qsort(int *a,int low,int high){
 	if(low>=high) return;
@@ -41,7 +70,7 @@ class Qsort:public Allsort{
 	public:
 		void sort(int *a,int size){
 			qsort(a,0,size);
-			cout<<"quick"<<endl;
+			cout<<"this is quick sort"<<endl;
 		}
 };
 
@@ -79,7 +108,7 @@ class Bubblesort:public Allsort{
 						a[i + 1] = temp;
 					}
 				} 
-			cout<<"this is bubble"<<endl;
+			cout<<"this is bubble sort"<<endl;
 		}
 };
 
@@ -102,6 +131,7 @@ void Mysort::display(){
 
 	for(int i=0;i<10;i++)
 		cout<<a[i]<<" ";
+	cout<<endl;
 }
 void Mysort::perform(){
 	s->sort(a,N);
@@ -122,7 +152,9 @@ int main(){
 	b.setsort(new Selectsort());
 	b.perform();
 	b.display();
-
+	b.setsort(new Headsort());
+	b.perform();
+	b.display();
 	return 0;
 }
 
